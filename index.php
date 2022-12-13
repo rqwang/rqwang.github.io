93c557ba30745a5e71a88e343956dacb70ccaff1
Roger Wang <rogerwang@Rogers-MacBook-Pro.local>
2022-12-12 23:20:36 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fnil\fcharset0 Consolas;}
+{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red241\green241\blue241;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?php header( 'Location: /index.html' ) ;  ?>}
